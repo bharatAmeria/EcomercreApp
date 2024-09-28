5534fa749d78fc7da6ae9d63f1618cda0056443c
bharatAmeria <21001019007@jcboseust.ac.in>
2024-09-28 12:58:20 +0530

    tests are added

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,26 +1,33 @@
-import express, {Request, Response} from 'express';
-import cors from 'cors';
+import express, { Request, Response } from "express";
+import cors from "cors";
 import "dotenv/config";
-import mongoose from 'mongoose';
+import mongoose from "mongoose";
 import userRoutes from "./routes/users";
-import authRoutes from "./routes/auth"
+import authRoutes from "./routes/auth";
 import cookieParser from "cookie-parser";
+
+if (!process.env.MONGODB_CONNECTION_STRING) {
+  throw new Error(
+    "MONGODB_CONNECTION_STRING is not defined in environment variables"
+  );
+}
 
 mongoose.connect(process.env.MONGODB_CONNECTION_STRING as string);
 
 const app = express();
-app.use(cookieParser());   
-app.use(express.json())
-app.use(express.urlencoded({extended: true}))
-app.use(cors({
- origin: process.env.FRONTEND_URL,
- credentials: true,
-}))
- 
+app.use(cookieParser());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(
+  cors({
+    origin: process.env.FRONTEND_URL,
+    credentials: true,
+  })
+);
 
 app.use("/api/auth", authRoutes);
 app.use("/api/users", userRoutes);
 
 app.listen(7002, () => {
- console.log("Server sunning on localhost: 7002")
+  console.log("Server sunning on localhost: 7002");
 });
